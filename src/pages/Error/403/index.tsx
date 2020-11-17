07b584476f2c1b263b3076824712887cc826a493
MuteDev <mutedev@mutedevs.nl>
2020-11-18 00:23:32 +0100

    Created a 403 error page idk why

---
+++ src/pages/Error/403/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../../../components/Button'
+import CenterText from '../../../components/CenterText'
+import ForbiddenImage from '../../../assets/403.gif'
+
+import '../style.scss';
+
+export default () => (
+  <CenterText>
+    <CenterText>Woah! That's forbidden!</CenterText>
+    <img className='Image' src={ForbiddenImage} />
+    <Button center to='/'>
+      Return to Home
+    </Button>
+  </CenterText>
+)
