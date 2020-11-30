06e1a7e722613b065711d1289668af65591a5959
MuteDev <mutedev@mutedevs.nl>
2020-11-30 14:22:14 +0100

    Partners, Rules, Staff and About updated

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,15 +1,69 @@
 import React, { Fragment } from 'react'
-
-// Import used assets
-import pinkbanner from '../../assets/banner-pink.png'
+import Button from '../../components/Button'
+import CenterText from '../../components/CenterText'
 
 export default () => {
   return (
     <Fragment>
+      <CenterText>
+        <h2>What is SubSilence</h2>
+      </CenterText>
+      <br />
       <p>
-        <code>SubSilence</code> yes!
+        SubSilence is a small but growing community of people all around the
+        world. Most of the acticity takes place in the{' '}
+        <code>
+          <a href='/discord'>Discord</a>
+        </code>{' '}
+        server but we also do stuff in real life and create content for the web.
       </p>
-      <img src={pinkbanner} />
+      <br />
+      <p>
+        Some of our members also stream on{' '}
+        <code>
+          <a href='https://twitch.tv'>Twitch</a>
+        </code>{' '}
+        to try and get more members SubSilence is supposed to be a nice and
+        comfortable place for everyone. That is why we have a few rules in
+        place. This is only so that we can keep the bad stuff out of the
+        community and have no worries about things like hate speech, racism
+        along other stuff.
+      </p>
+      <br />
+      <p>
+        Currently there are a few services that SubSilence offers, some only to
+        members of the{' '}
+        <code>
+          <a href='/discord'>SubSilence Discord Server</a>
+        </code>{' '}
+        as updates will be sent in a channel to keep everyone who uses the
+        service up to date. Things such as the SubSilence Minecraft server are
+        limited to only people who are active in the Discord server so we can
+        better see if something could go wrong.
+      </p>
+      <br />
+      <p>
+        Charmy is the Discord bot we created for the SubSilence Discord Server.
+        The bot will mkae things like moderating specific channels a lot easier.
+        The bot also features the verification system that we use to make sure
+        we do not get raided. For more information about Charmy you can visit
+        her{' '}
+        <code>
+          <a href='/charmy'>webpage</a>
+        </code>
+        .
+      </p>
+      <br />
+      <p>
+        Everything that is related to SubSilence is something that we made
+        ourselves. This costs quite a bit of money sometimes. That is why we
+        will accept donations. Every penny of these donations help us to keep
+        hosting the website, charmy, the miencraft server among a lot of other
+        things.
+      </p>
+      <Button center to='/'>
+        Return to Home
+      </Button>
     </Fragment>
   )
 }
--- src/pages/Discord/Partners/partners.ts
+++ src/pages/Discord/Partners/partners.ts
@@ -8,23 +8,29 @@ const partners: Partners[] = [
       'https://cdn.discordapp.com/icons/552533572200955914/08f465245824b5d68f48fadfc8a2276a.webp?size=1024'
   },
   {
+    name: "App's Haven",
+    quote: 'A small piece of home in a far away land',
+    img:
+      'https://cdn.discordapp.com/icons/609721695892144129/20f23a15b17dae7d0d5e3673d682738c.webp?size=1024'
+  },
+  {
     name: 'Bakje Woater',
     quote: 'Lekker drinkon',
     img:
-      'https://cdn.discordapp.com/icons/723138973823926383/ae152a4837325ca5ee52323dde502980.webp?size=1024',
+      'https://cdn.discordapp.com/icons/723138973823926383/ae152a4837325ca5ee52323dde502980.webp?size=1024'
   },
   {
     name: 'LolicePrison',
     quote: 'A prison full of criminals',
     img:
-      'https://cdn.discordapp.com/icons/744682911252349021/42b5c76ff61286d9d1753e68e5027b17.webp?size=1024',
+      'https://cdn.discordapp.com/icons/744682911252349021/42b5c76ff61286d9d1753e68e5027b17.webp?size=1024'
   },
   {
     name: 'HebHonger',
     quote: 'The start of it all',
     img:
-      'https://cdn.discordapp.com/icons/427207115141152803/a12d528ba7dba4915d4433fa4be6c4d3.webp?size=1024',
-  },
+      'https://cdn.discordapp.com/icons/427207115141152803/a12d528ba7dba4915d4433fa4be6c4d3.webp?size=1024'
+  }
 ]
 
 export default partners
