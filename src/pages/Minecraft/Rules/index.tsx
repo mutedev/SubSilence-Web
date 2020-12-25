4b1047449c3ceaa63f21001b6acc9e9077d0a5b8
LuximusHunter <thomaslipman10@gmail.com>
2020-12-25 17:10:44 +0100

    add server rules page

--- src/pages/Minecraft/Rules/index.tsx
+++ src/pages/Minecraft/Rules/index.tsx
@@ -1,26 +1,25 @@
 import React, { Fragment } from 'react'
-import CenterText from '../../../components/CenterText'
 import Button from '../../../components/Button'
-import MinecraftCard from '../../../components/MinecraftCard'
+import rules from './rules'
 import './style.scss'
 
 export default () => {
   return (
     <Fragment>
-      <CenterText>
-        <h1>
-          Join our <code>Minecraft</code> server!
-        </h1>
-      </CenterText>
-      <MinecraftCard url='mc.subsilence.nl:15973' version='1.16.4' />
-      <Button center to='/minecraft/rules'>
-        Please read the rules
-      </Button>
-      <Button center to='/minecraft/map'>
-        View the world map
-      </Button>
-      <Button center to='/'>
-        Return to Home
+      <table>
+        <tbody>
+          {rules.map((rule: string, index: number) => (
+            <tr className='Rule' key={index}>
+              <td className='Index'>
+                <code>[ {index + 1} ]</code>
+              </td>
+              <td className='Text'>{rule}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Button center to='/minecraft'>
+        Back
       </Button>
     </Fragment>
   )
